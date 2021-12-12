ecdbf7d7e68c4b7d8b302ff5815977da952df11f
Umid Sirinov <sirinovumid623@gmail.com>
2021-12-12 15:18:39 +0400

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private StoreContext _storeContext { get; set; }
+
+
+        public ProductsController(StoreContext storeContext )
+        {
+
+            _storeContext = storeContext;
+
+        }
+
+        [HttpGet]
+        public List<Product> GetDAta()
+        {
+            var data = _storeContext.Products.ToList();
+
+
+
+            return data;
+        }
+    }
+}
