32fb30f1d6dda41c10e39f15c2a4804736d0d2d5
Umid Sirinov <sirinovumid623@gmail.com>
2021-12-17 16:57:23 +0400

    Add BaseEntity  ProductType and ProductBrand

--- API.Core/DbModels/Product.cs
+++ API.Core/DbModels/Product.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace API.Core.DbModels
 {
-    public class Product
+    public class Product:BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureURL { get; set; }
+        public ProductType ProducType { get; set; }
+        public int ProductId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
+
+
     }
 }
