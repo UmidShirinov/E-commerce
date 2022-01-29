514f1176ff4704981cb515b489e306cd4b2463c3
Umid Sirinov <sirinovumid623@gmail.com>
2022-01-29 18:00:58 +0400

    Add specification class to GenericREpository

--- API.Infrastructure/Implements/GenericRepository.cs
+++ API.Infrastructure/Implements/GenericRepository.cs
@@ -1,6 +1,8 @@
 ﻿using API.Core.DbModels;
 using API.Core.Interfaces;
+using API.Core.Specifications;
 using API.Infrastructure.DataContext;
+using API.Infrastructure.Queries;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -23,9 +25,25 @@ namespace API.Infrastructure.Implements
             return await _storeContext.Set<T>().FindAsync(id);
         }
 
+      
+
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _storeContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_storeContext.Set<T>().AsQueryable(), spec);
         }
     }
 }
